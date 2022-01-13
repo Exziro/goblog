e07bfae756efe38ce4a040e13122ba7646b96110
yixi qi <qiyixi2010@gmail.com>
2022-01-13 22:41:17 +0900

    加入中间件

--- main.go
+++ main.go
@@ -8,18 +8,18 @@ import (
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	//w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Hello, 欢迎来到 goblog</h1>")
 
 }
 func notFoundHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	//w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusNotFound)
 	fmt.Fprint(w, "<h1>请求页面未找到 :(</h1><p>如有疑惑，请联系我们。</p>")
 }
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset= utf-8")
+	//w.Header().Set("Content-Type", "text/html; charset= utf-8")
 	fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
 		"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
 }
@@ -34,6 +34,16 @@ func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
 func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "创建新文章")
 }
+
+//中间件处理，用于设置所有页面适配请求头的处理模式
+func forceHTMLMiddleware(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		//设置标头
+		w.Header().Set("Content-type", "text/html; charset=utf-8")
+		//继续处理请求
+		h.ServeHTTP(w, r)
+	})
+}
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/", homeHandler).Methods("GET").Name("home")
@@ -43,6 +53,7 @@ func main() {
 	router.HandleFunc("/articles", articlesStoreHandler).Methods("POST").Name("articles.store")
 	//自定义404
 	router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
+	router.Use(forceHTMLMiddleware)
 	//通过命名路由获取URL
 	homeURL, _ := router.Get("home").URL()
 	fmt.Println("HomeURL:", homeURL)
