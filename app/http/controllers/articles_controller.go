1b987f8d0a8889a765a397bed0e1d105033b07d0
yixi qi <qiyixi2010@gmail.com>
2022-01-23 14:50:22 +0900

    重构+美化表单页面

--- app/http/controllers/articles_controller.go
+++ app/http/controllers/articles_controller.go
@@ -13,7 +13,6 @@ import (
 	"goblog/pkg/route"
 
 	"net/http"
-	"text/template"
 
 	"gorm.io/gorm"
 )
@@ -24,7 +23,7 @@ type ArticlesController struct {
 //ArticlesFormData 创建博文表单数据
 type ArticlesFormData struct {
 	Title, Body string
-	URL         string
+	Article     article.Article
 	Errors      map[string]string
 }
 
@@ -52,7 +51,7 @@ func (*ArticlesController) Show(w http.ResponseWriter, r *http.Request) {
 		}
 	} else {
 		//读取成功渲染模板
-		view.Render(w, "articles.show", article)
+		view.Render(w, article, "articles.show")
 		//fmt.Fprint(w, "读取文章成功"+article.Title)
 	}
 	//fmt.Fprint(w, "文章ID："+id)
@@ -70,7 +69,7 @@ func (*ArticlesController) Index(w http.ResponseWriter, r *http.Request) {
 	} else {
 		//加载模板
 		//设置模板相对路径
-		view.Render(w, "articles.index", articles)
+		view.Render(w, articles, "articles.index")
 	}
 }
 
@@ -92,26 +91,18 @@ func (*ArticlesController) Store(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusInternalServerError)
 			fmt.Fprint(w, "500服务器内部错误")
 		}
-		fmt.Fprint(w, "验证通过！")
-		fmt.Fprintf(w, "title 的值为: %v <br>", title)
-		fmt.Fprintf(w, "title 的长度为: %v <br>", utf8.RuneCountInString(title))
-		fmt.Fprintf(w, "body 的值为: %v <br>", body)
-		fmt.Fprintf(w, "body 的长度为: %v <br>", utf8.RuneCountInString(body))
+		// fmt.Fprint(w, "验证通过！")
+		// fmt.Fprintf(w, "title 的值为: %v <br>", title)
+		// fmt.Fprintf(w, "title 的长度为: %v <br>", utf8.RuneCountInString(title))
+		// fmt.Fprintf(w, "body 的值为: %v <br>", body)
+		// fmt.Fprintf(w, "body 的长度为: %v <br>", utf8.RuneCountInString(body))
 
 	} else {
-		storeURL := route.Name2URL("articles.store")
-
-		data := ArticlesFormData{
+		view.Render(w, ArticlesFormData{
 			Title:  title,
 			Body:   body,
-			URL:    storeURL,
 			Errors: errors,
-		}
-		tmpl, err := template.ParseFiles("resources/views/articles/create.gohtml")
-		logger.LogError(err)
-
-		err = tmpl.Execute(w, data)
-		logger.LogError(err)
+		}, "articles.show", "articles._form_field")
 
 		// fmt.Fprintf(w, "有错误发生，errors 的值为: %v <br>", errors)
 	}
@@ -131,21 +122,7 @@ func (*ArticlesController) Store(w http.ResponseWriter, r *http.Request) {
 
 //Create 文章创建
 func (*ArticlesController) Create(w http.ResponseWriter, r *http.Request) {
-	storeURL := route.Name2URL("articles.store")
-	data := ArticlesFormData{
-		Title:  "",
-		Body:   "",
-		URL:    storeURL,
-		Errors: nil,
-	}
-	tmpl, err := template.ParseFiles("resources/views/articles/create.gohtml")
-	if err != nil {
-		panic(err)
-	}
-	err = tmpl.Execute(w, data)
-	if err != nil {
-		panic(err)
-	}
+	view.Render(w, ArticlesFormData{}, "articles.create", "articles._form_field")
 
 }
 func validateArticleFromData(title, body string) map[string]string {
@@ -169,7 +146,7 @@ func (*ArticlesController) Edit(w http.ResponseWriter, r *http.Request) {
 	// id := vars["id"]
 	id := route.GetRouterVariable("id", r)
 	//读取文章数据
-	article, err := article.Get(id)
+	_article, err := article.Get(id)
 	// article := Article{}
 	// querry := "SELECT * FROM articles WHERE id = ?"
 	// err := db.QueryRow(querry, id).Scan(&article.ID, &article.Title, &article.Body)
@@ -186,17 +163,13 @@ func (*ArticlesController) Edit(w http.ResponseWriter, r *http.Request) {
 
 	} else {
 		// 4. 读取成功，显示编辑文章表单
-		updateURL := route.Name2URL("articles.update", "id", id)
-		data := ArticlesFormData{
-			Title:  article.Title,
-			Body:   article.Body,
-			URL:    updateURL,
-			Errors: nil,
-		}
-		tmp, err := template.ParseFiles("resources/views/articles/edit.gohtml")
-		logger.LogError(err)
-		err = tmp.Execute(w, data)
-		logger.LogError(err)
+
+		view.Render(w, ArticlesFormData{
+			Title:   _article.Title,
+			Body:    _article.Body,
+			Article: _article,
+			Errors:  nil,
+		}, "articles.edit", "articles._form_field")
 	}
 }
 
@@ -241,18 +214,12 @@ func (*ArticlesController) Update(w http.ResponseWriter, r *http.Request) {
 
 		} else {
 			//表单验证不通过，显示理由
-			updateURL := route.Name2URL("articles.update", "id", id)
-			data := ArticlesFormData{
-				Title:  title,
-				Body:   body,
-				Errors: errors,
-				URL:    updateURL,
-			}
-			tmpl, err := template.ParseFiles("resources/views/articles/edit.gohtml")
-			logger.LogError(err)
-			err = tmpl.Execute(w, data)
-			logger.LogError(err)
-			fmt.Fprintf(w, "更新失败")
+			view.Render(w, ArticlesFormData{
+				Title:   title,
+				Body:    body,
+				Article: _article,
+				Errors:  errors,
+			}, "articles.edit", "articles._form_field")
 		}
 	}
 
--- pkg/view/view.go
+++ pkg/view/view.go
@@ -10,21 +10,23 @@ import (
 )
 
 //Render 渲染视图
-func Render(w io.Writer, name string, data interface{}) {
+func Render(w io.Writer, data interface{}, tplFiles ...string) {
 	//设置模板相对路径
 	viewDir := "resources/views/"
 	//2. 语法糖 将article.show更正为 articles/show
-	name = strings.Replace(name, ".", "/", -1)
+	for i, f := range tplFiles {
+		tplFiles[i] = viewDir + strings.Replace(f, ".", "/", -1) + ".gohtml"
+	}
 	//3 所有布局模板文件 Slice
-	files, err := filepath.Glob(viewDir + "layouts/*.gohtml")
+	layoutFiles, err := filepath.Glob(viewDir + "layouts/*.gohtml")
 	logger.LogError(err)
-	//4 在Slice里新增文件
-	newfiles := append(files, viewDir+name+".gohtml")
+	//4 在Slice里新增文件 合并所有文件
+	allFiles := append(layoutFiles, tplFiles...)
 	//5 解析所有模板
-	tmpl, err := template.New(name + ".gohtml").
+	tmpl, err := template.New("").
 		Funcs(template.FuncMap{
 			"RouteName2URL": route.Name2URL,
-		}).ParseFiles(newfiles...)
+		}).ParseFiles(allFiles...)
 	logger.LogError(err)
 	err = tmpl.ExecuteTemplate(w, "app", data)
 	logger.LogError(err)
